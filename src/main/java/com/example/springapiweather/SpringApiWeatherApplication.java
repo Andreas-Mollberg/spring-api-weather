85091d2948a3856a9128360cd31fd5ce94c55af7
AndreasM85 <147496978+AndreasM85@users.noreply.github.com>
2023-12-19 20:40:29 +0100

    Created project

---
+++ src/main/java/com/example/springapiweather/SpringApiWeatherApplication.java
@@ -0,0 +1,13 @@
+package com.example.springapiweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiWeatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springapiweather/SpringApiWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springapiweather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiWeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
